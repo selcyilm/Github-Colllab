79ab245052c395e84ab5b18693d6f02a1e6a3bf4
Ömer Gürsel <grsl.omer@gmail.com>
2024-02-23 18:55:23 +0100

    Update

--- src/Ömer/Q10_Degistir.java
+++ src/Ömer/Q10_Degistir.java
@@ -1,2 +1,23 @@
-package Ömer;public class Q10_Degistir {
+package Ömer;
+
+import java.util.Scanner;
+
+public class Q10_Degistir {
+
+    public static String degistir(String kelime) {
+        String ters = "";
+        for (int i = kelime.length() - 1; i >= 0; i--) {
+            ters += kelime.charAt(i);
+        }
+        return ters;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir kelime yaziniz : ");
+        String kelime = scanner.nextLine();
+
+        System.out.println(degistir(kelime));
+    }
 }
